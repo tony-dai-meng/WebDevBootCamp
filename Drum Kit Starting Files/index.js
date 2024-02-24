9b7bbc0f6a831955deda4cc3e47adf6ba534a121
Tony Dai <tony.dai.meng@gmail.com>
2024-02-25 10:18:27 +1100

    completed drum kit challenge

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,49 @@
+const buttonAll = document.querySelectorAll(".drum");
+
+for (var i = 0; i < buttonAll.length; i++) {
+    buttonAll[i].addEventListener("click", function () {
+        // Get the first class name
+        var className = this.classList[0];
+        var aud = new Audio();
+        switch (className) {
+            case "w":
+                aud.setAttribute("src", "./sounds/crash.mp3");
+                aud.play();
+                break;
+            case "a":
+                aud.setAttribute("src", "./sounds/kick-bass.mp3");
+                aud.play();
+                break;
+            case "s":
+                aud.setAttribute("src", "./sounds/snare.mp3");
+                aud.play();
+                break;
+            case "d":
+                aud.setAttribute("src", "./sounds/tom-1.mp3");
+                aud.play();
+                break;
+            case "j":
+                aud.setAttribute("src", "./sounds/tom-2.mp3");
+                aud.play();
+                break;
+            case "k":
+                aud.setAttribute("src", "./sounds/tom-3.mp3");
+                aud.play();
+                break;
+            default:
+                aud.setAttribute("src", "./sounds/tom-4.mp3");
+                aud.play();
+                break;
+        }
+    }
+    );
+}
+
+document.addEventListener("keydown", function(e){
+    try{
+        document.querySelector("."+String(e.key)).click()
+    }
+    catch(exep){
+        console.log("No sound there champ! "+ exep)
+    }
+})
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This repository is created to track the personal Web development journey for Tony Dai.
-THe repositiory tracks Angela Yu's popular web development course on Udemy called "The Complete 2024 Web Development Bootcamp" as of the 24th of Feb, 17:59.
-Some excercises are missing from that course in this repo however that would be most likely because i've moved pass it or it was lateent knowledge through my formal training with IT gained through my degree in Mechatronics, this is espically true for the algorithm section.
+The repositiory tracks Angela Yu's popular web development course on Udemy called "The Complete 20xx Web Development Bootcamp" as of the 24th of Feb 2024, 17:59.
+Some excercises are missing from that course in this repo however that would be most likely because i've moved passed it or it was latent knowledge through my formal training with the software discipline gained through my degree in Mechatronics, this is especially true for the algorithms section.

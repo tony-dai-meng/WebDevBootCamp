0509f3c16ac4212391498bd3a4071a68eee6372a
Tony Dai <S65861@gmail.com>
2024-06-23 14:01:45 +1000

    index changes

--- Backend/3.3 Postman/index.js
+++ Backend/3.3 Postman/index.js
@@ -10,12 +10,12 @@ const port = 3000;
 // *********************
 
 app.get("/", (req, res) => {
-  res.send("<h1>Home Page</h1>");
+  res.status(200).send("<h1>Home Page</h1>");
 });
 
 app.post("/register", (req, res) => {
   //Do something with the data
-  res.sendStatus(201).send("Processing");
+  res.sendStatus(201);
 });
 
 app.put("/user/angela", (req, res) => {

6892d9589adba6990a3e9a9c483d89b690c90593
Tony Dai <S65861@gmail.com>
2024-08-13 07:21:21 +1000

    JSON excercise

--- BlogCapstoneProject/index.js
+++ BlogCapstoneProject/index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3001;
-
+let a = [];
 /* Write your code here:
 Step 1: Render the home page "/" index.ejs
 Step 2: Make sure that static files are linked to and the CSS shows up.
@@ -12,11 +12,12 @@ Step 3: Add the routes to handle the render of the about and contact pages.
 Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs",{a:a});
 });
 
 app.get("/create", (req, res) => {
@@ -24,7 +25,8 @@ app.get("/create", (req, res) => {
 });
 
 app.post("/create", (req, res, next) => {
-
+  a.push(req.body);
+  res.redirect('/')
 })
 
 app.get("/contact", (req, res) => {
